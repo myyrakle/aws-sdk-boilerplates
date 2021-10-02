ab0acb13179157dc52916bead736ce2da77a9146
myyrakle <sssang97@naver.com>
2021-10-02 22:05:11 +0900

    Update version.js

--- lambda/version.js
+++ lambda/version.js
@@ -1,3 +1,8 @@
+const aws = require('aws-sdk');
+// import aws from 'aws-sdk';
+
+const lambda = new aws.Lambda();
+
 async function getCurrentVersionByAlias(alias) {
     const data = await lambda.getAlias({
         FunctionName: 'express_test',
