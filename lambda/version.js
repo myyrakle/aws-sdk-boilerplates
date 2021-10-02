ad834ae7606569c4c95ed0f5709be630b913e5cc
myyrakle <sssang97@naver.com>
2021-10-02 22:05:48 +0900

    Update version.js

--- lambda/version.js
+++ lambda/version.js
@@ -3,6 +3,7 @@ const aws = require('aws-sdk');
 
 const lambda = new aws.Lambda();
 
+// 특정 함수 별칭의 버전 획득
 async function getCurrentVersionByAlias(alias) {
     const data = await lambda.getAlias({
         FunctionName: 'express_test',
